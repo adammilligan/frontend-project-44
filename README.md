56b9f9fe134b5c74362cabf4067b114fdf62593e
Вадим Валыгин <vadim12388@mail.ru>
2022-09-20 17:14:53 +0300

    add brain-even

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/adammilligan/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/adammilligan/frontend-project-44/actions)
 <a href="https://codeclimate.com/github/adammilligan/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/a2cab5273d68b3f26b17/maintainability" /></a>
+https://asciinema.org/connect/bac5c58d-a34f-4b7d-970e-63f2eed472a3
---
+++ bin/brain-even.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import { hello } from '../src/cli.js';
+const name = hello ();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const doComparison = (checkNumber) => {
+    if (checkNumber % 2 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
+};
+
+let i = 0;
+while (i < 3) {
+    let checkNumber = Math.round(Math.random() * 100);
+    console.log(`Question: ${checkNumber}`);
+    let result = doComparison(checkNumber);
+    const answer = readlineSync.question('Your answer: ');
+    if (result === answer.toLowerCase()) {
+        i += 1;
+        console.log('Correct!');
+    } else {
+        console.log(`'${answer.toLowerCase()}' is wrong answer ;(. Correct answer was '${result}'
+Let's try again, ${name}!`);
+        break;
+    };
+}
+if (i === 3) {
+    console.log(`Congratulations, ${name}!`);
+};
+
+
+
