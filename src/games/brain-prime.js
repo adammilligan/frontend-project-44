d1df26c4c6607b9fcb28a966cc8a302210869f27
Вадим Валыгин <vadim12388@mail.ru>
2022-10-03 15:59:01 +0300

    new test

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,8 +1,8 @@
-import createRandomNumber from '../utils.js';
+import randomNumber from '../utils.js';
 import startGame from '../index.js';
 
-const maxNumber = 100;
-const minNumber = 1;
+const MAX_NUMBER = 100;
+const MIN_NUMBER = 1;
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrimeNum = (num) => {
@@ -16,11 +16,11 @@ const isPrimeNum = (num) => {
 
 export default () => {
   const comparisonData = () => {
-    const checkNumber = createRandomNumber(maxNumber, minNumber);
+    const checkNumber = randomNumber(MAX_NUMBER, MIN_NUMBER);
     const result = isPrimeNum(checkNumber) ? 'yes' : 'no';
     return {
       result,
-      question: String(checkNumber),
+      question: `${checkNumber}`,
     };
   };
   startGame(comparisonData, gameRule);
