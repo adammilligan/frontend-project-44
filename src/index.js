e434257422948414cdba5f61299d259d998c475b
Вадим Валыгин <vadim12388@mail.ru>
2022-10-04 17:29:09 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export default (generateCondition, gameRule) => {
     if (result === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
